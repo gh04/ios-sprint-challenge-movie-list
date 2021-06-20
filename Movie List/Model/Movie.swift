f9797d835e9ce8aad2eac184203da6b3a88882a8
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-19 17:02:18 -0700

    Added view controllers files and set the scenes class in IB

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Gerardo Hernandez on 6/19/21.
+//  Copyright © 2021 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    let seen: Bool = false
+}
---
+++ Movie List/Views/MovieTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Gerardo Hernandez on 6/19/21.
+//  Copyright © 2021 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+  
+
+}
